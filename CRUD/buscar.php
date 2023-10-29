535f4c7fc649c419c284071fd38dfcf302ab9992
David Arrebola <d.arrebola.porras@gmail.com>
2023-10-29 10:13:56 +0100

    falta consultar

--- CRUD/buscar.php
+++ CRUD/buscar.php
@@ -13,21 +13,24 @@ if(!isset($_SESSION["usuario"])){
 </head>
 <body>
     <form action="#" method="post">
-        <label for="nombre">Nombre :</label>
+        <label for="titulo">Titulo :</label>
         <input type="text" name="titulo" id="">
         <label for="Autor">Autor :</label>
         <input type="text" name="autor" id="">
         <input type="submit" value="Buscar">
     </form>
+    
 </body>
 </html>
 <?php
+
     if(isset($_POST["titulo"])&&isset($_POST["autor"])){
         $titulo=$_POST["titulo"];
         $autor=$_POST["autor"];
-
+        //si los campos de texto estan vacios, mostrará un mensaje de que uno de los campos debe estar compeltado
         if(empty($titulo)&&empty($autor)){
             echo "<h2>Introduce un titulo o un autor para poder buscar</h2>";
+            //si el titulo esta vacio, buscaremos por autor
         }else if(empty($titulo)){
             include("conexion.php");
             $conexion=crear_conexion();
@@ -45,7 +48,25 @@ if(!isset($_SESSION["usuario"])){
                     echo $valor["codigo"]." - ".$valor["titulo"]." - ".$valor["autor"]." - ".$valor["disponible"]."<br>";
                 }
             }
-
+        }else if(empty($autor)){
+            include("conexion.php");
+            $conexion=crear_conexion();
+            //preparo la consulta de busqueda por titulo
+            $consultaTitulo="SELECT * FROM libros WHERE titulo =:titulo;";
+            $stmtT=$conexion->prepare($consultaTitulo);
+            $stmtT->bindParam(":titulo",$titulo);
+            $stmtT->execute();
+            $resultado=$stmtT->rowCount();
+            if($resultado==0){
+                echo "<h2>No se han encontrado libros con ese titulo</h2>";
+            }else{
+                $fila=$stmtT->fetchAll(PDO::FETCH_ASSOC);
+                foreach($fila as $campo=>$valor){
+                    echo $valor["codigo"]." - ".$valor["titulo"]." - ".$valor["autor"]." - ".$valor["disponible"]."<br>";
+                }
+            }
         }
     }//llave del primer isset
+    
+    echo "<a href='menu.php'>Volver al menu</a><br>";
 ?>
