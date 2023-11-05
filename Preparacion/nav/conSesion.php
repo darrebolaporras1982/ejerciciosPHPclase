647e960f66fe6cfeeb61fc192d23096e3665dacd
David Arrebola <d.arrebola.porras@gmail.com>
2023-11-05 11:38:58 +0100

    un poco de estilo

--- Preparacion/nav/conSesion.php
+++ Preparacion/nav/conSesion.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/Preparacion/style.css">
 </head>
 <body>
+<div class="inicio">
     <nav>
         <ul>
             <li><a href="index.php">INICIO</a></li>
@@ -16,3 +17,4 @@
 
         </ul>
     </nav>
+</div>
